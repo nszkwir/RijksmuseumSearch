d856e8826641fb0626fd676cfdf17cf1faf190d3
Nicolas Szkwir <nicoszkwir@gmail.com>
2021-12-06 21:39:14 +0100

    - added navigation graph

--- app/src/main/java/com/nszkwir/rijksmuseumsearch/presentation/MainActivity.kt
+++ app/src/main/java/com/nszkwir/rijksmuseumsearch/presentation/MainActivity.kt
@@ -2,18 +2,34 @@ package com.nszkwir.rijksmuseumsearch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.nszkwir.rijksmuseumsearch.R
+import com.nszkwir.rijksmuseumsearch.databinding.MainActivityBinding
 import com.nszkwir.rijksmuseumsearch.presentation.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun showProgressBar() {
+        binding.clProgressBar.visibility = View.VISIBLE
+        binding.coordinator.alpha = DIM_BACKGROUND_ALPHA
+    }
+
+    fun hideProgressBar() {
+        binding.clProgressBar.visibility = View.GONE
+        binding.coordinator.alpha = VISIBLE_BACKGROUND_ALPHA
+    }
+
+    companion object {
+        const val DIM_BACKGROUND_ALPHA = 0.75f
+        const val VISIBLE_BACKGROUND_ALPHA = 1f
     }
 }
