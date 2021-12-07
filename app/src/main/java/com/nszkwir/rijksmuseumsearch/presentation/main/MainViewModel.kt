374c9299cedb539a9299c59ddf26b85994eb4723
Nicolas Szkwir <nicoszkwir@gmail.com>
2021-12-07 17:04:18 +0100

    - implemented MainViewModel event handling - implemented MainViewModel keywordSearch - added MainViewModel livedata

--- app/src/main/java/com/nszkwir/rijksmuseumsearch/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/nszkwir/rijksmuseumsearch/presentation/main/MainViewModel.kt
@@ -1,13 +1,58 @@
 package com.nszkwir.rijksmuseumsearch.presentation.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.nszkwir.rijksmuseumsearch.common.utils.Event
 import com.nszkwir.rijksmuseumsearch.domain.MainUseCasesImpl
+import com.nszkwir.rijksmuseumsearch.domain.models.ArtObject
+import com.nszkwir.rijksmuseumsearch.domain.result_data.ArtObjectKeywordSearchResult
 import com.nszkwir.rijksmuseumsearch.presentation.core.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val mainUseCases: MainUseCasesImpl
 ) : BaseViewModel() {
-    // TODO: Implement the ViewModel
+
+    // TODO implement a MainViewState class to observe from fragment instead observig data
+    private val _artObjects = MutableLiveData<Event<ArrayList<ArtObject>>>()
+    val artObjects: LiveData<Event<ArrayList<ArtObject>>> = _artObjects
+
+    private val _searchError = MutableLiveData<Event<String>>()
+    val searchError: LiveData<Event<String>> = _searchError
+
+    fun processViewEvent(viewEvent: MainViewEvent) {
+        when (viewEvent) {
+            is MainViewEvent.ErrorMessage -> {
+                _snackbarError.value = Event(viewEvent.message)
+            }
+            is MainViewEvent.KeywordSearch -> {
+                doKeywordSearch(viewEvent.query)
+            }
+            is MainViewEvent.LoadingView -> {
+                _loading.value = Event(viewEvent.isLoading)
+            }
+        }
+    }
+
+    private fun doKeywordSearch(query: String) {
+        viewModelScope.launch {
+            val searchResult = mainUseCases.artObjectKeywordSearch(query)
+            when (searchResult) {
+                is ArtObjectKeywordSearchResult.Success -> {
+                    _artObjects.value = Event(searchResult.data)
+                }
+                is ArtObjectKeywordSearchResult.Error -> {
+                    _searchError.value = Event("Error")
+                }
+                is ArtObjectKeywordSearchResult.NetworkError -> {
+                    _searchError.value = Event("Network Error")
+                }
+            }
+        }
+    }
+
 }
