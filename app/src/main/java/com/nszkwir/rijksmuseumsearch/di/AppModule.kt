ba7eaf8e14b682a1ba959795e43a2b9a44c8727c
Nicolas Szkwir <nicoszkwir@gmail.com>
2021-12-07 12:46:13 +0100

    - added providers for DI

--- app/src/main/java/com/nszkwir/rijksmuseumsearch/di/AppModule.kt
+++ app/src/main/java/com/nszkwir/rijksmuseumsearch/di/AppModule.kt
@@ -1,6 +1,10 @@
 package com.nszkwir.rijksmuseumsearch.di
 
-import com.nszkwir.rijksmuseumsearch.domain.MainUseCases
+import com.nszkwir.rijksmuseumsearch.common.network.ApiClient
+import com.nszkwir.rijksmuseumsearch.data.art_objects.repository.ArtObjectRepository
+import com.nszkwir.rijksmuseumsearch.data.art_objects.repository.ArtObjectRepositoryImpl
+import com.nszkwir.rijksmuseumsearch.data.art_objects.repository.ArtObjectService
+import com.nszkwir.rijksmuseumsearch.domain.MainUseCasesImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,7 +17,25 @@ object AppModule {
 
     @Provides
     @Singleton
+    fun provideArtObjectRepository(
+        service: ArtObjectService
+    ): ArtObjectRepository =
+        ArtObjectRepositoryImpl(service)
+
+    @Provides
+    @Singleton
     fun provideMainUseCases(
-    ): MainUseCases =
-        MainUseCases()
+        repository: ArtObjectRepository
+    ): MainUseCasesImpl =
+        MainUseCasesImpl(repository)
+
+    @Provides
+    @Singleton
+    fun provideApiClient(): ApiClient =
+        ApiClient()
+
+    @Provides
+    @Singleton
+    fun provideArtObjectService(apiClient: ApiClient): ArtObjectService =
+        apiClient.createService(ArtObjectService::class.java)
 }
